d9caae30395e81e37ea422f8611b21326345df53
a-sayed123 <a.marraey@gmail.com>
2025-09-20 17:24:12 +0300

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,44 +1,44 @@
 import os
+# secret_word
+# challenge word variable 
 
-def clear_screen():
-              # Cond for eny operating system 
-    os.system("cls" if os.name == "nt" else
-"clear")
-
-# Clear screen to start with n clean place
-clear_screen()
-
-# The challenge word
-secret_word = "hello"
-
-# Player guessing word in this variable 
-guess = ""
-
-# Number of used hints
-guess_count = 0
-
+# guess_limit = 3
 # Number of available hints
-guess_limit = 3
 
-# Variable alarm you that player is finished hints
-out_of_guesses = False
+def GuessingSecretWord(secret_word, guess_limit):
 
-# game loop end if win and not win
-while guess != secret_word and not out_of_guesses:
-    if guess_count < guess_limit:
-        guess = input("\n enter the word : \n")
-        guess_count += 1
+    def clear_screen():
+                  # Cond for any operating system 
+        os.system("cls" if os.name == "nt" else "clear")
+
+    # Clear screen to start with n clean place
+    clear_screen()
+
+    # Player guessing word in this variable 
+    guess = ""
+
+    # Number of used hints
+    guess_count = 0
+
+    # Variable to alert that player is out of hints
+    out_of_guesses = False
+
+    # game loop end if win and not win
+    while guess != secret_word and not out_of_guesses:
+        if guess_count < guess_limit:
+            guess = input("\n enter the word : \n").lower()
+            guess_count += 1
+        else:
+            out_of_guesses = True
+        if not guess == secret_word:
+            print(f"\n wrong word, you have {guess_limit - guess_count} attempts left.\n")
+    clear_screen()
+
+    # Game over notification 
+    if out_of_guesses:
+        print("\n you lose out of guesses \n")
     else:
-        out_of_guesses = True
-    if not guess == secret_word:
-        print("\n wrong word, try again \n")
-clear_screen()
-
-# game over notification 
-if out_of_guesses:
-    print("\n you lose out of guesses \n")
-else:
-    print("\n you win, congratulations \n")
+        print("\n you win, congratulations \n")
 
 
 
