145e0d3778fa78662be6f95f2ea5feff7c87df47
a-sayed123 <a.marraey@gmail.com>
2025-09-20 16:38:05 +0300

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,10 +1,9 @@
 import os
 
-
 def clear_screen():
-    os.system("cls" if os.name == "nt" else "clear")
-
-
+    os.system("cls" if os.name == "nt" else
+"clear")
+
 clear_screen()
 secret_word = "skinet"
 guess = ""
